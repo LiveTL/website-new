a6fbf3b283f1270d55490065228e9b8fa65d725e
DaniruKun <thedanpetrov@gmail.com>
2023-06-09 00:05:27 +0200

    Add background image to YtcFilter page

--- src/routes/ytcfilter/+page.svelte
+++ src/routes/ytcfilter/+page.svelte
@@ -1,4 +1,12 @@
-<div class="hero min-h-screen bg-info">
+<script lang="ts">
+  import YtcDemoBackground from '$lib/assets/ytcfilter/embedded-mode.png';
+</script>
+
+<div
+  class="hero min-h-screen"
+  style="background-image: url({YtcDemoBackground})"
+>
+  <div class="absolute inset-0 bg-black opacity-40" />
   <div class="hero-content text-center">
     <div class="max-w-md">
       <h1 class="text-5xl font-bold text-base-100">YtcFilter</h1>
