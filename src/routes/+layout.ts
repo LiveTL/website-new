8c540db202e40928f72e6af1a91e28aad50a3fe2
Daniils Petrovs <daniils@platogo.com>
2023-11-05 18:09:26 +0100

    Implement locale switcher

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,9 +2,11 @@ import type { LayoutLoad } from './$types';
 
 import { loadTranslations } from '$lib/translation';
 
+export const ssr = false;
+
 export const load = (async ({ url }) => {
   const { pathname } = url;
-  const initLocale = 'en';
+  const initLocale = localStorage.getItem('lang') || 'en';
 
   await loadTranslations(initLocale, pathname);
   return {};
