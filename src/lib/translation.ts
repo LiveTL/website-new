9fd295ef68a93d0524ddc06d0537e6df01ea6934
Daniils Petrovs <daniils@platogo.com>
2023-11-05 18:29:05 +0100

    Add jp localisation

--- src/lib/translation.ts
+++ src/lib/translation.ts
@@ -68,9 +68,63 @@ const config = {
     // Japanese
     {
       locale: 'ja',
+      key: 'common',
+      loader: async () =>
+        (await import('./translations/ja/common.json')).default,
+    },
+    {
+      locale: 'ja',
       key: 'home',
       loader: async () => (await import('./translations/ja/home.json')).default,
     },
+    {
+      locale: 'ja',
+      key: 'about',
+      routes: ['/about'],
+      loader: async () =>
+        (await import('./translations/ja/about.json')).default,
+    },
+    {
+      locale: 'ja',
+      key: 'contact',
+      routes: ['/contact'],
+      loader: async () =>
+        (await import('./translations/ja/contact.json')).default,
+    },
+    {
+      locale: 'ja',
+      key: 'faq',
+      routes: ['/faq'],
+      loader: async () => (await import('./translations/ja/faq.json')).default,
+    },
+    {
+      locale: 'ja',
+      key: 'privacy',
+      routes: ['/privacy'],
+      loader: async () =>
+        (await import('./translations/ja/privacy.json')).default,
+    },
+    {
+      locale: 'ja',
+      key: 'ytcfilter',
+      routes: ['/ytcfilter'],
+      loader: async () =>
+        (await import('./translations/ja/ytcfilter.json')).default,
+    },
+    {
+      locale: 'ja',
+      key: 'livetl',
+      routes: ['/livetl', '/livetl/about', '/livetl/install'],
+      loader: async () =>
+        (await import('./translations/ja/livetl.json')).default,
+    },
+    {
+      locale: 'ja',
+      key: 'hyperchat',
+      routes: ['/hyperchat', '/hyperchat/about', '/hyperchat/install'],
+      loader: async () =>
+        (await import('./translations/ja/hyperchat.json')).default,
+    }
   ],
 
   preprocess: 'preserveArrays',
