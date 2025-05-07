5260281c8c1a8e68b4f0b340fb59703fc7dd01ea
arkon <4098258+arkon@users.noreply.github.com>
2025-05-07 10:01:35 -0400

    Update Discord invite link
    Fixes #16

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
-export const DISCORD_SERVER_URL = 'https://discord.com/invite/uJrV3tmthg';
+export const DISCORD_SERVER_URL = 'https://discord.com/invite/fZZasP9bj3';
 export const GITHUB_ORG_URL = 'https://github.com/LiveTL';
